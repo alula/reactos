788dd5b6b5f06d7e025cbf90322099f09a30d0ae
Ahmed ARIF <arif.ing@outlook.com>
2026-04-21 21:13:58 +0200

    [DS]WDMGUID][PCI][E1000E] Fix release-build link: anchor GUID_ACPI_PCI_INTERFACE in wdmguid

--- sdk/lib/wdmguid/wdmguid.c
+++ sdk/lib/wdmguid/wdmguid.c
@@ -17,4 +17,15 @@ DEFINE_GUID(GUID_DEVINTERFACE_CDROM,
 DEFINE_GUID(GUID_DEVINTERFACE_PARTITION,
   0x53f5630aL, 0xb6bf, 0x11d0, 0x94, 0xf2, 0x00, 0xa0, 0xc9, 0x1e, 0xfb, 0x8b);
 
+/*
+ * ACPI/PCI device interface. Mirrored from
+ * <reactos/drivers/acpi/acpipci.h> here to give every driver that links
+ * wdmguid a real definition of GUID_ACPI_PCI_INTERFACE. Without this,
+ * release builds (-O2/-O3) strip the DECLSPEC_SELECTANY storage emitted
+ * by DEFINE_GUID in each consumer and the link fails with
+ * "undefined reference to GUID_ACPI_PCI_INTERFACE".
+ */
+DEFINE_GUID(GUID_ACPI_PCI_INTERFACE,
+  0xA7E9DB84L, 0xDB4C, 0x4289, 0x87, 0x6B, 0xE2, 0xC3, 0xE6, 0x3B, 0x5F, 0x4F);
+
 /* EOF */
