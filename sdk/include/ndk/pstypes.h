40a5e1572146211fb6eddad60323d500836ff3ef
Ahmed ARIF <arif.ing@outlook.com>
2026-05-16 22:34:32 +0200

    [NDK] Remove packing from PS_JOB_TOKEN_FILTER

--- sdk/include/ndk/pstypes.h
+++ sdk/include/ndk/pstypes.h
@@ -1767,7 +1767,6 @@ typedef struct _PS_JOB_TOKEN_FILTER
     PLUID_AND_ATTRIBUTES CapturedPrivileges;
     ULONG CapturedPrivilegesLength;
 } PS_JOB_TOKEN_FILTER, *PPS_JOB_TOKEN_FILTER;
-#include <poppack.h>
 
 //
 // Executive Job (EJOB)
